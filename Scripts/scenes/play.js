66a885dd108b4c1358329a05417f7892315724d2
Sean Smith <srsmith92@gmail.com>
2016-03-04 16:36:40 -0500

    Added Dice to screen

--- Scripts/scenes/play.js
+++ Scripts/scenes/play.js
@@ -16,13 +16,22 @@ var scenes;
         // Start Method
         Play.prototype.start = function () {
             //Add Play Label
-            this._playLabel = new objects.Label("PLAY SCENE", "60px Consolas", "#000000", config.Screen.CENTER_X, config.Screen.CENTER_Y);
+            this._playLabel = new objects.Label("PLAY SCENE", "60px Consolas", "#000000", config.Screen.CENTER_X, config.Screen.CENTER_Y - 200);
             this.addChild(this._playLabel);
             //Add Rolll Button to the Play Scene
             this._rollButton = new objects.Button("RollButton", config.Screen.CENTER_X, config.Screen.CENTER_Y + 180);
             this.addChild(this._rollButton);
             //add Event listener for Roll button
             this._rollButton.on("click", this._rollButtonClick, this);
+            //Initialize Hand
+            this._hand = new Array(); //instantiate reels bitmap 
+            for (var die = 0; die < 2; die++) {
+                this._hand[die] = new createjs.Bitmap(assets.getResult("One"));
+                this._hand[die].x = 193 + (die * 120);
+                this._hand[die].y = 217;
+                this.addChild(this._hand[die]);
+                console.log("Die" + die + " " + this._hand[die]);
+            }
             // add this scene to the global stage container
             stage.addChild(this);
         };
--- Scripts/scenes/play.ts
+++ Scripts/scenes/play.ts
@@ -4,6 +4,7 @@ module scenes {
         //PRIVATE INSTANCE VARIABLES ++++++++++++
         private _playLabel: objects.Label;
         private _rollButton: objects.Button;
+        private _hand:createjs.Bitmap[]; //array for both dice
         
         // CONSTRUCTOR ++++++++++++++++++++++
         constructor() {
@@ -20,7 +21,7 @@ module scenes {
             this._playLabel = new objects.Label(
                 "PLAY SCENE","60px Consolas", 
                 "#000000", 
-                config.Screen.CENTER_X,config.Screen.CENTER_Y);
+                config.Screen.CENTER_X,config.Screen.CENTER_Y - 200);
             this.addChild(this._playLabel);
 
 
@@ -33,6 +34,19 @@ module scenes {
             
             //add Event listener for Roll button
             this._rollButton.on("click", this._rollButtonClick, this);
+            
+            //Initialize Hand
+            this._hand = new Array<createjs.Bitmap>();//instantiate reels bitmap 
+            for (var die:number = 0; die < 2; die++)
+            {
+                this._hand[die] = new createjs.Bitmap(assets.getResult("One"));
+                this._hand[die].x = 193 + (die * 120);
+                this._hand[die].y = 217;
+  
+                this.addChild(this._hand[die]);
+                
+                console.log("Die" + die + " " + this._hand[die]);
+            }
 
             // add this scene to the global stage container
             stage.addChild(this);
